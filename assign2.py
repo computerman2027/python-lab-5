3659d0b64eef16bb7637e8cade7c737d02f253d3
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-08-31 15:18:35 +0530

    Update assign2.py

--- assign2.py
+++ assign2.py
@@ -4,7 +4,7 @@
 st=input("Enter a string : ")
 if(len(st.strip())==0):
     print("No string entered")
-elif(st==st[::-1]):
+elif(st.strip()==st.strip()[::-1]):
     print("Palindrome string")
 else:
     print("Not Palindrome string")
